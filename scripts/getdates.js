29811a3f51cfe01bf9e466201f4619c264e3363e
lucianitoseguel <lucianitoseguel@gmail.com>
2024-09-17 02:22:20 +0000

    2.1 upload myself

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+
+//Last modified
+let oLastModif = new Date(document.lastModified);
+
+let lastmod = document.getElementById('lastm')
+
+lastmod.textContent =  `Last Modified: ${document.lastModified}`
