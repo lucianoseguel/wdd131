59ab2438fac800c6430bf1472b15e242389c1a78
Luciano Seguel <lucianitoseguel@gmail.com>
2024-10-04 18:09:20 -0300

    grid template weather and data

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,4 @@
+
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#menu');
 
@@ -11,3 +12,25 @@ hambutton.addEventListener('click', () => {
 let lastmod = document.getElementById('lastm')
 
 lastmod.textContent =  `Last Modified: ${document.lastModified}`
+
+
+
+
+let windchilltext = document.getElementById("windchill")
+
+function calculateWindChill(temperature, windSpeed) {
+    // Wind Chill Formula
+    const windChill = 13.12 + 0.6215 * temperature 
+                    - 11.37 * Math.pow(windSpeed, 0.16) 
+                    + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
+    
+    return windChill.toFixed(2); // Redondea a 2 decimales
+}
+
+// Ejemplo con temperatura de 5°C y viento de 15 km/h
+const temperature = 5;
+const windSpeed = 14;
+
+const windChill = calculateWindChill(temperature, windSpeed);
+
+windchilltext.textContent = `Wind Chill: ${windChill} °C`
