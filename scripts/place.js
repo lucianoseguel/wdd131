7ddd9dedc29e26f7750afbdc07707c32a0f95e0f
Luciano Seguel <lucianitoseguel@gmail.com>
2026-02-14 15:46:49 -0300

    .

--- scripts/place.js
+++ scripts/place.js
@@ -1,11 +1,5 @@
 
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
 
-hambutton.addEventListener('click', () => {
-	mainnav.classList.toggle('show');
-	hambutton.classList.toggle('show');
-});
 
 
 let lastmod = document.getElementById('lastm')
